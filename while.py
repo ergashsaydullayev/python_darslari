2b4bbee5c52b87d88e42be2d62628e2b063e713b
ergashsaydullayev <saydullayevergash600@gmail.com>
2023-10-07 17:00:40 +0500

    kodlar

---
+++ while.py
@@ -0,0 +1,22 @@
+# a=0
+# n=int(input("n ni kirit>>"))
+# while a<=n:
+#     print(a)
+#     a+=1
+# print("finish")
+
+
+# n=int(input("n ga son kirit"))
+# a=0
+# while n>=a:
+#     print(n)
+#     n=n-1
+# print("finish")
+# 7
+
+n=int(input("n ga son kirit>>"))
+a=int(input("a ga son kirit>>"))
+while n<=a:
+    print(n)
+    n+=2
+print("finish")
