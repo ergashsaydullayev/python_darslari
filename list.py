a6e53d7ecb2f835bddc9e32dd47e181082bfe99a
ergashsaydullayev <saydullayevergash600@gmail.com>
2023-10-10 18:58:39 +0500

    lisdagi kodlar yozilgan

---
+++ list.py
@@ -0,0 +1,22 @@
+# a=[1,2,3,4]
+# print(a[0])
+
+# a=["x", 1,"y",2,"z",3]
+# print(a[0])
+
+
+# list1=[1,2,3,4]
+# list2=[5,6,7,8]
+# print(list1+list2)
+
+# list1=['a', 1, 'b', 2, 'c', 3, 'd', 4]
+# Output: ['a', 1, 'b']
+# Constraints:
+
+# a=['a',1,'b',2,'c',3,'d',4]
+# print(a[1:4])
+# Input: list1=['a', 1, 'b', 2, 'c', 3, 'd', 4] n = 2
+# Output: [4, 3, 2, 1]
+list1=['a', 1, 'b', 2, 'c', 3, 'd', 4]
+print(list1[::-2])
+
